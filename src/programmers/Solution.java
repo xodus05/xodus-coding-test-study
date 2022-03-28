a24fbd4f5638b1955555bb98938da0b01c762576
xodus05 <82927278+xodus05@users.noreply.github.com>
2022-03-28 22:57:28 +0900

    0328 odd even (#4)
    * [ADD] 0328 OddEven
    
    * [UPDATE] 주석

---
+++ src/programmers/Solution.java
@@ -0,0 +1,12 @@
+package programmers;
+
+public class Solution {
+    public String solution(int num) {
+        if(num%2==0) //짝수일경우
+            return "Even";
+        else if(num%2==1) //홀수일경우
+            return "Odd";
+        String answer = "";
+        return answer;
+    }
+}
