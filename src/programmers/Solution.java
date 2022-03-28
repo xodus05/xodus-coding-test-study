7f492973449a6fa65029e1e3be5d47df36b28746
xodus05 <82927278+xodus05@users.noreply.github.com>
2022-03-28 15:12:26 +0900

    [ADD] 0328 OddEven

---
+++ src/programmers/Solution.java
@@ -0,0 +1,12 @@
+package programmers;
+
+public class Solution {
+    public String solution(int num) {
+        if(num%2==0)
+            return "Even";
+        else if(num%2==1)
+            return "Odd";
+        String answer = "";
+        return answer;
+    }
+}
