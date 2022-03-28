cbd451e126bcc7a2f8fae28cfa515a9fb6f214eb
xodus05 <s2121@e-mirim.hs.kr>
2022-03-28 22:56:39 +0900

    [UPDATE] 주석

--- src/programmers/Solution.java
+++ src/programmers/Solution.java
@@ -2,9 +2,9 @@ package programmers;
 
 public class Solution {
     public String solution(int num) {
-        if(num%2==0)
+        if(num%2==0) //짝수일경우
             return "Even";
-        else if(num%2==1)
+        else if(num%2==1) //홀수일경우
             return "Odd";
         String answer = "";
         return answer;
