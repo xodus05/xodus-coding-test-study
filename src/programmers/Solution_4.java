03fbd71e2dd8f816a184438e8cadd55626e50cb5
xodus05 <82927278+xodus05@users.noreply.github.com>
2022-04-01 14:43:07 +0900

    [ADD] 0401 정수 제곱근 판별 (#12)

---
+++ src/programmers/Solution_4.java
@@ -0,0 +1,20 @@
+package programmers;
+
+class Solution_4 {
+    public static long solution(long n) {
+        long answer = 0;
+        long a = (long)Math.sqrt(n);
+
+        if(Math.pow(a,2)==n)
+            answer = (long)Math.pow(a+1,2);
+        else
+            answer = -1;
+
+        return answer;
+    }
+    public static void main(String[] args){
+        System.out.println(solution(3));
+        System.out.println(solution(121));
+        System.out.println(solution(4));
+    }
+}
